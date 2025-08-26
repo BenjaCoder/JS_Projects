c9c64d546946ef5793e4a44faa27964b159bb066
BenjaCoder <ben.hamula@gmail.com>
2025-08-26 18:50:44 -0400

    Removes 85% and travel, adds 5 mins per session logic

--- mozaic_time_keeping/script.js
+++ mozaic_time_keeping/script.js
@@ -1,48 +1,38 @@
 window.onload = () => {
     const count30 = document.getElementById("session-30");
     const count45 = document.getElementById("session-45");
-    const transport = document.getElementById("transportation");
     const total30 = document.getElementById("total-30");
     const total45 = document.getElementById("total-45");
-    const totalMin = document.getElementById("totalMin");
-    const of85 = document.getElementById("of85");
-    const reportTime = document.getElementById("report-time");
-    const reportMath = document.getElementById("report-math");
+    const totalMin = document.getElementById("total-min");
+    const resetButton = document.getElementById("reset");
 
     count30.addEventListener("input", function() {
-        const minutes = this.value * 30 + count45.value * 45;
+        const minutes = this.value * 30 + count45.value * 45 +
+            ((Number(count30.value) + Number(count45.value)) * 5);
         totalMin.innerHTML = `${minutes} minutes`;
-        of85.innerHTML = Math.ceil(minutes / 0.85);
         total30.innerHTML = `(${this.value * 30})`;
-        const reportHM = 450 - Math.ceil(minutes / 0.85) - transport.value;
-        reportTime.innerHTML = reportHM;
-        reportMath.innerHTML = `(450 - ${Math.ceil(minutes / 0.85)} - ${transport.value})`;
-        const reportHours = Math.floor(reportHM / 60);
-        const reportMinutes = reportHM - (reportHours * 60);
-        document.getElementById("report-time-hours").innerHTML = `${reportHours} hours, ${reportMinutes} minutes`;
+        totalMin.innerHTML +=
+            `<br />(${count30.value * 30} + ${count45.value * 45} +
+            (${Number(count30.value) + Number(count45.value)} * 5))`
     });
 
     count45.addEventListener("input", function() {
-        const minutes = this.value * 45 + count30.value * 30;
+        const minutes = this.value * 45 + count30.value * 30 +
+            ((Number(count30.value) + Number(count45.value)) * 5);
         totalMin.innerHTML = `${minutes} minutes`;
-        of85.innerHTML = Math.ceil(minutes / 0.85);
         total45.innerHTML = `(${this.value * 45})`;
-        const reportHM = 450 - Math.ceil(minutes / 0.85) - transport.value;
-        reportTime.innerHTML = reportHM;
-        reportMath.innerHTML = `(450 - ${Math.ceil(minutes / 0.85)} - ${transport.value})`;
-        const reportHours = Math.floor(reportHM / 60);
-        const reportMinutes = reportHM - (reportHours * 60);
-        document.getElementById("report-time-hours").innerHTML = `${reportHours} hours, ${reportMinutes} minutes`;
+        totalMin.innerHTML +=
+            `<br />(${count30.value * 30} + ${count45.value * 45} +
+            (${Number(count30.value) + Number(count45.value)} * 5))`
     });
 
-    transport.addEventListener("input", function() {
-        const minutes = count30.value * 30 + count45.value * 45;
-        const of85 = Math.ceil(minutes / 0.85);
-        const reportHM = 450 - Math.ceil(minutes / 0.85) - transport.value;
-        reportTime.innerHTML = reportHM;
-        reportMath.innerHTML = `(450 - ${of85} - ${this.value})`;
-        const reportHours = Math.floor(reportHM / 60);
-        const reportMinutes = reportHM - (reportHours * 60);
-        document.getElementById("report-time-hours").innerHTML = `${reportHours} hours, ${reportMinutes} minutes`;
+    resetButton.addEventListener("click", function() {
+        count30.value = "";
+        total30.innerHTML = "";
+        count45.value = "";
+        total45.innerHTML = "";
+        totalMin.innerHTML = "";
+        count30.focus();
     });
+    
 }
